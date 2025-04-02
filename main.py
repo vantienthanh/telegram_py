5e1b92dd2f98547dff74fa92be7cb6aa3a64179e
van.tien.thanh <van.tien.thanh@sun-asterisk.com>
2025-04-02 17:50:19 +0700

    init project

---
+++ main.py
@@ -0,0 +1,25 @@
+from opentele.td import TDesktop
+from opentele.tl import TelegramClient
+from opentele.api import API, UseCurrentSession
+import asyncio
+
+async def main():
+
+    # Load TDesktop client from tdata folder
+    tdataFolder = r"input/thanhvt-main/tdata"
+    tdesk = TDesktop(tdataFolder)
+    
+    # Check if we have loaded any accounts
+    assert tdesk.isLoaded()
+
+    # flag=UseCurrentSession
+    #
+    # Convert TDesktop to Telethon using the current session.
+    client = await tdesk.ToTelethon(session="telethon.session", flag=UseCurrentSession)
+    
+    # Connect and print all logged-in sessions of this client.
+    # Telethon will save the session to telethon.session on creation.
+    await client.connect()
+    await client.PrintSessions()
+
+asyncio.run(main())
